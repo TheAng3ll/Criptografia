18a27d23b98dc397ca1f299c1a96fcfe7336f293
Angel Barrera <144848292+TheAng3ll@users.noreply.github.com>
2023-09-13 15:48:39 -0600

    cifrado.java

--- cifrado.java
+++ cifrado.java
@@ -9,37 +9,55 @@ import java.util.Scanner;
 class cifrado
 {
 
-	public String cifrar(String frase) //Funcion para el cifrado
+	public  String cifrar(String frase, int positiones) //Funcion para el cifrado
 	{
-		frase="HOLA";
 		StringBuilder mensajeCifrado = new StringBuilder();
-
 		char l,l1,c;
-		
-		//String[] abecedario = {"A","B","C","D","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+		int p;
+
 		String abecedario = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
 		for (int i=0; i<frase.length(); i++ ) 
 		{
 			l=frase.charAt(i);
-			for(int j=0;j<25; j++)
+			for(int j=0;j<26; j++)
 			{
 				l1=abecedario.charAt(j);
 				if(l==l1)
 				{
-					int p=j+5;
-					c = abecedario.charAt(p);
-					mensajecifrado.append(c);
+					if(j+positiones>26)
+					{
+						p=j+positiones-26;
+						c = abecedario.charAt(p);
+						mensajeCifrado.append(c);
+
+					}
+				 	if(j+positiones<=26)
+					{
+					
+						System.out.println(l1);
+						p=j+positiones;
+						c = abecedario.charAt(p);
+						mensajeCifrado.append(c);
+
+					}
+				}
+				
+				else
+				{
+
 				}
 			}
 		}
 
-		return "hola"; //mensaje crifrado
+		System.out.println(mensajeCifrado);
+		return mensajeCifrado.toString(); //mensaje crifrado
 	}
 
 	public static void main(String[] args) 
 	{
-		Scanner scr = new Scanner(System.in); // creamos un scanner para ingresar datos
-		cifrado mjs = new cifrado();
+		Scanner scr = new Scanner(System.in);// creamos un scanner para ingresar datos
+		Scanner texto = new Scanner(System.in); // creamos un scanner para ingresar datos
+		cifrado mjs = new cifrado(); //se instancia la funcion de cifrado
 		String m;
 		int n;
 
@@ -47,7 +65,12 @@ class cifrado
 		System.out.println("Cuantos espacios quieres el desplazamiento? ");
 		n = scr.nextInt();
 		System.out.println("Dime que mensaje quieres cifrar?");
-		m = scr.nextLine();
-		mjs.cifrar(m);
+		m = texto.nextLine();
+		String textocifrado = mjs.cifrar(m,n);
+		mjs.cifrar(m,n);
+		
+		
+
 	}
 }
+
