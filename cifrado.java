ef30e7a01193ddf53072c23c1b49a4db3576fe3d
Angel Barrera <144848292+TheAng3ll@users.noreply.github.com>
2023-09-12 23:05:13 -0600

    Add files via upload

---
+++ cifrado.java
@@ -0,0 +1,23 @@
+import java.awt.*;
+import javax.swing.*;
+
+class cifrado
+{
+	public cifrado( )
+	{
+
+	}
+
+	public void cifrar(String frase)
+	{
+		String[] abecedario = {"A","B","C","D","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+		for (int i; i<frase.lenght;i++ ) {
+			
+		}
+	}
+
+	public static void main(String[] args) 
+	{
+		
+	}
+}
